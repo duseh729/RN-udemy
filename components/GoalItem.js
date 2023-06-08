a1f3fcfe3f38b67d3b615145182cb8c56de746d0
DoYeon Ahn <duseh729@naver.com>
2023-06-08 15:38:20 +0900

    feat: goal리스트 터치 시 삭제 구현

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,12 +2,21 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 const GoalItem = (props) => {
   return (
-    // bind()는 javascript 내장 함수이다. 공부가 필요해보임**
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      {/* Pressable은 터치가 가능하게 하는 컴포넌트 */}
+      <Pressable
+        // 안드로이드 전용 스타일
+        android_ripple={{ color: "#3e028d" }}
+        // bind()는 javascript 내장 함수이다. 공부가 필요해보임**
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        // ios 전용 스타일
+        style={({ pressed }) => {
+          pressed && styles.pressedItem;
+        }}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -16,11 +25,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
+    padding: 8,
     color: "white",
   },
 });
